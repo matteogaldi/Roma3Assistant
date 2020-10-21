217d3c7cb843ebea10d6845b8ad21c0d5c552b77
Gaggo007 <72210028+Gaggo007@users.noreply.github.com>
2020-10-21 11:55:18 +0200

    Base progetto main/barra sotto
    Main Page/ Barra sotto con scritte e loghi

---
+++ App/Roma3Assistant/Roma3Assistant/Menu.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.PlatformConfiguration;
+using Xamarin.Forms.PlatformConfiguration.AndroidSpecific;
+using Xamarin.Forms.Xaml;
+
+namespace Roma3Assistant
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Menu : Xamarin.Forms.TabbedPage
+    {
+        public Menu()
+        {
+            InitializeComponent();
+            On<Android>().SetToolbarPlacement(ToolbarPlacement.Bottom);
+        }
+    }
+}
---
+++ App/Roma3Assistant/Roma3Assistant/Profilo.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Roma3Assistant
+{
+    public partial class Profilo : ContentPage
+    {
+        public Profilo()
+        {
+            InitializeComponent();
+        }
+    }
+}
