9cdf4a79a6634e6cd6330a9ae0a2a3f7aa1c23ae
piebaroni <pietroba99@gmail.com>
2020-10-22 13:04:16 +0200

    pagina principale impostazioni

---
+++ App/Roma3Assistant/Roma3Assistant/ImpostazioniBase.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Roma3Assistant
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ImpostazioniBase : ContentPage
+    {
+        public ImpostazioniBase()
+        {
+            InitializeComponent();
+        }
+
+        private void profiloUtente_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void security_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void credits_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void assistenza_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
