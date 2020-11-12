a50ccfc78a6b69e180881d8c165ae46dac8fd537
Matteo Galdi <git@matteogaldi.dev>
2020-11-12 16:56:47 +0100

    Update app.js

--- API/app.js
+++ API/app.js
@@ -3,7 +3,6 @@ const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 require("dotenv").config();
 
-const indexRouter = require("./routes/index");
 const coursesRouter = require("./routes/courses");
 const subscriptionsRouter = require("./routes/subscriptions");
 
@@ -14,8 +13,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.use("/", indexRouter);
-app.use("/corsi", coursesRouter);
+app.use("/courses", coursesRouter);
 app.use("/subscriptions", subscriptionsRouter);
 
 module.exports = app;
