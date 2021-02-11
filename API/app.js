a6760789adb105bf52ae76111b262d1cbe8d70cf
Matteo Galdi <mat.galdi1@stud.uniroma3.it>
2021-02-11 18:21:58 +0100

    * Add new routes to API * Add helmet

--- API/app.js
+++ API/app.js
@@ -1,12 +1,17 @@
 const express = require("express");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
+const helmet = require("helmet");
 require("dotenv").config();
 
 const coursesRouter = require("./routes/courses");
 const subscriptionsRouter = require("./routes/subscriptions");
+const userRouter = require("./routes/user");
+const tokenRouter = require("./routes/token");
+const teachersRouter = require("./routes/teachers");
 
 const app = express();
+app.use(helmet());
 
 app.use(logger("dev"));
 app.use(express.json());
@@ -15,5 +20,8 @@ app.use(cookieParser());
 
 app.use("/courses", coursesRouter);
 app.use("/subscriptions", subscriptionsRouter);
+app.use("/user", userRouter);
+app.use("/auth/login", tokenRouter);
+app.use("/teachers", teachersRouter);
 
 module.exports = app;
