4c0b1bd73b9069418bf00d1ab4c0a78a7de6f06c
piebaroni <pietroba99@gmail.com>
2021-02-15 11:18:55 +0100

    Logout/cambio colori

--- App/Roma3Assistant/Roma3Assistant/Sicurezza.xaml.cs
+++ App/Roma3Assistant/Roma3Assistant/Sicurezza.xaml.cs
@@ -3,23 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
-
-
 
 namespace Roma3Assistant
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class PaginaUtente : ContentPage
+    public partial class Sicurezza : ContentPage
     {
-        public PaginaUtente()
+        public Sicurezza()
         {
             InitializeComponent();
-            //BindingContext = new PaginaUtenteViewModel(Navigation);
         }
 
-        private void logout_Clicked(object sender, EventArgs e)
+        private void cambioPSW_Clicked(object sender, EventArgs e)
         {
 
         }
