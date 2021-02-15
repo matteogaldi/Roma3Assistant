f55c00fe971d29d08130abb860effc3942bccf7a
Matteo Galdi <git@matteogaldi.dev>
2021-02-15 11:47:14 +0100

    add models
    Add models to deserialize APIs response

---
+++ App/Roma3Assistant/Roma3Assistant/Models/Course.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace Roma3Assistant.Models
+{
+    public class Course
+    {
+        public string Name { get; set; }
+        public int Partition { get; set; }
+        public string Teacher { get; set; }
+        public int cfu { get; set; }
+        public int code { get; set; }
+        public string id { get; set; }
+        public Teacher Teachers { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Name} {Teachers.Name}";
+        }
+    }
+}
