e38e049d39c155796ba4e8dad7a1b4fe820d94bc
Matteo Galdi <git@matteogaldi.dev>
2021-02-18 13:30:33 +0100

    Update Course.cs
    Newtonsoft compliant

--- App/Roma3Assistant/Roma3Assistant/Models/Course.cs
+++ App/Roma3Assistant/Roma3Assistant/Models/Course.cs
@@ -1,19 +1,41 @@
-﻿using System;
+﻿using Newtonsoft.Json;
 namespace Roma3Assistant.Models
 {
     public class Course
     {
+        [JsonProperty("name")]
         public string Name { get; set; }
+
+        [JsonProperty("partition")]
         public int Partition { get; set; }
+
+        [JsonProperty("teacher")]
         public string Teacher { get; set; }
+
+        [JsonProperty("cfu")]
         public int Cfu { get; set; }
+
+        [JsonProperty("code")]
         public int Code { get; set; }
+
+        [JsonProperty("id")]
         public string Id { get; set; }
-        public Teacher Teachers { get; set; }
+
+        [JsonProperty("teachers")]
+        public Teacher TeacherInfo { get; set; }
+
+        [JsonProperty("notes")]
+        public string Notes { get; set; }
+
+        [JsonProperty("exams")]
+        public string Exams { get; set; }
+
+        [JsonProperty("schedule")]
+        public string Schedule { get; set; }
 
         public override string ToString()
         {
-            return $"{Name} {Teachers.Name}";
+            return $"{Name} {TeacherInfo.Name}";
         }
     }
 }
